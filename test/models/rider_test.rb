e951b69c8a34feda24b9638245735c88af7709fd
mikesmall <michael.l.a.small@gmail.com>
2017-07-04 14:20:59 -0400

    Completed 'Part 3: Using the Rider Factory'

--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -5,9 +5,9 @@ class RiderTest < ActiveSupport::TestCase
   def test_that_riders_swing
     # Use your knowledge of factories to make the test pass!
     # this time use create( ... ) instead of build
-    aegon = nil
-    daenerys = nil
-
+    aegon = create(:rider, name: "Aegon", sword: "Blackfyre", nickname: "Aegster", email: "knight_moves@hotmail.com")
+    daenerys = create(:rider, name: "Daenerys", sword: "Lightbringer", nickname: "Danny", email: "giddy_up@lycos.com")
+    # binding.pry
     # can't change the assert_ statements!
     skip unless aegon && daenerys
     assert_equal('Daenerys brings Lightbringer crashing down!', daenerys.swing)
