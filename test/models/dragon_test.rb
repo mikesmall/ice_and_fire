62f46611895638ce7e10c1ca795362de65704351
mikesmall <michael.l.a.small@gmail.com>
2017-07-04 13:52:08 -0400

    Reached point of 9 runs, 6 assertions, 0 failures, 0 errors, 3 skips

--- test/models/dragon_test.rb
+++ test/models/dragon_test.rb
@@ -19,8 +19,8 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_young_dragons_from_asoiaf
     # use your knowledge of factories to make this test pass!
-    young_dragon = nil
-
+    young_dragon = build(:dragon, age: 2)
+    # binding.pry
     # not allowed to change the assert statement!
     skip unless young_dragon
     assert_equal( 'A Song of Ice and Fire', young_dragon.period_alive)
@@ -28,7 +28,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_middle_dragons_from_targ_dynasty
     # use your knowledge of factories to make this test pass!
-    middle_dragon = nil
+    middle_dragon = build(:dragon, age: 20)
 
     # not allowed to change the assert statement!
     skip unless middle_dragon
@@ -37,7 +37,7 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_ancient_dragons_from_age_of_valyria
     # use your knowledge of factories to make this test pass!
-    ancient_dragon = nil
+    ancient_dragon = build(:dragon, age: 415)
 
     # not allowed to change the assert statement!
     skip unless ancient_dragon
